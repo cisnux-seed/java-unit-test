8a76f63abc12569fc044de73ffde4e8dde678ae9
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-02-28 02:37:35 +0700

    add optiona nullable to get method

--- src/main/java/dev/cisnux/unittest/service/PersonService.java
+++ src/main/java/dev/cisnux/unittest/service/PersonService.java
@@ -3,6 +3,7 @@ package dev.cisnux.unittest.service;
 import dev.cisnux.unittest.data.Person;
 import dev.cisnux.unittest.repository.PersonRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public class PersonService {
@@ -14,12 +15,10 @@ public class PersonService {
 
     public Person get(String id) {
         final var person = repository.selectPersonById(id);
-        if (person == null)
-            throw new IllegalArgumentException("Person not found");
-        return person;
+        return Optional.ofNullable(person).orElseThrow(() -> new IllegalArgumentException("Person not found"));
     }
 
-    public Person register(String name){
+    public Person register(String name) {
         final var person = new Person(UUID.randomUUID().toString(), name);
         repository.insert(person);
         return person;
